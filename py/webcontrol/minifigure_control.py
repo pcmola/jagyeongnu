785729f69f72e8f80685c254afa848bd7c600cff
pcmola <pcmola@naver.com>
2018-10-21 00:00:40 +0900

    Update minifigure_control.py

--- py/webcontrol/minifigure_control.py
+++ py/webcontrol/minifigure_control.py
@@ -1,15 +1,20 @@
 #minifigure_control.py
 import webiopi
 
+#import sys
+#sys.path.append('/home/pi/py/python-omxplayer-wrapper-develop')
+#sys.path.append('/usr/local/lib/python2.7/dist-packages/decorator-4.0.10-py2.7.egg')
+#from omxplayer import OMXPlayer
+
 webiopi.setDebug()
 
 GPIO = webiopi.GPIO
 
-jong_pwm_pin = 13
-jong_dir_pin = 19
+jong_pwm_pin = 19
+jong_dir_pin = 13
 book_pwm_pin = 16
-book_dir_pin = 12   
-jing_pwm_pin =  5
+book_dir_pin = 12
+jing_pwm_pin = 20
 jing_dir_pin =  6
 
 def setup():
@@ -46,55 +51,59 @@ def destroy():
 
 @webiopi.macro
 def jong_strike():
+    webiopi.debug("Jong Strike!!!")
     GPIO.output(jong_dir_pin, GPIO.LOW)
-    GPIO.pulseRatio(jong_pwm_pin, 0.2);
-    webiopi.sleep(0.6)
-    GPIO.pulseRatio(jong_pwm_pin, 0.65);
-    webiopi.sleep(0.3)
-    GPIO.pulseRatio(jong_pwm_pin, 0);
-    
-    webiopi.sleep(0.5)
+    GPIO.pulseRatio(jong_pwm_pin, 0.01); #delay
+    webiopi.sleep(0.60)
+    GPIO.pulseRatio(jong_pwm_pin, 0.18);
+    webiopi.sleep(0.19)
+
+    GPIO.pulseRatio(jong_pwm_pin, 0); #stop   
+    webiopi.sleep(0.8)
     
     GPIO.output(jong_dir_pin, GPIO.HIGH)
-    GPIO.pulseRatio(jong_pwm_pin, 0.54);
-    webiopi.sleep(0.4)
+    GPIO.pulseRatio(jong_pwm_pin, 0.18);    
+    webiopi.sleep(0.17)
+    
     GPIO.pulseRatio(jong_pwm_pin, 0);
 
     
 @webiopi.macro 
 def book_strike():
+    webiopi.debug("Book Strike!!!")
     GPIO.output(book_dir_pin, GPIO.LOW)
-    GPIO.pulseRatio(book_pwm_pin, 0.1);
-    webiopi.sleep(0.6)
-    GPIO.pulseRatio(book_pwm_pin, 0.75);
-    webiopi.sleep(0.3)
-    GPIO.pulseRatio(book_pwm_pin, 0);
+    GPIO.pulseRatio(book_pwm_pin, 0.01); #delay
+    webiopi.sleep(0.60)
+    GPIO.pulseRatio(book_pwm_pin, 0.18);
+    webiopi.sleep(0.19)
     
-    webiopi.sleep(0.5)
+    GPIO.pulseRatio(book_pwm_pin, 0); #stop
+    webiopi.sleep(0.8)
     
     GPIO.output(book_dir_pin, GPIO.HIGH)
-    GPIO.pulseRatio(book_pwm_pin, 0.60);
-    webiopi.sleep(0.3)
+    GPIO.pulseRatio(book_pwm_pin, 0.18);    
+    webiopi.sleep(0.17)
+    
     GPIO.pulseRatio(book_pwm_pin, 0);
     
     
             
 @webiopi.macro
 def jing_strike():
+    webiopi.debug("Jing Strike!!!")
     GPIO.output(jing_dir_pin, GPIO.LOW)
-    GPIO.pulseRatio(jing_pwm_pin, 0.2);
-    webiopi.sleep(0.7)
-    GPIO.pulseRatio(jing_pwm_pin, 0.50);
-    webiopi.sleep(0.2)
-    GPIO.pulseRatio(jing_pwm_pin, 0);
+    GPIO.pulseRatio(jing_pwm_pin, 0.01); #delay
+    webiopi.sleep(0.60)
+    GPIO.pulseRatio(jing_pwm_pin, 0.17);
+    webiopi.sleep(0.16)
     
-    webiopi.sleep(0.5)
+    GPIO.pulseRatio(jing_pwm_pin, 0); #stop
+    webiopi.sleep(0.8)
     
     GPIO.output(jing_dir_pin, GPIO.HIGH)
-    GPIO.pulseRatio(jing_pwm_pin, 0.48);
-    webiopi.sleep(0.2)
-    GPIO.pulseRatio(jing_pwm_pin, 0);
+    GPIO.pulseRatio(jing_pwm_pin, 0.17);    
+    webiopi.sleep(0.14)
     
-   
-       
+    GPIO.pulseRatio(jing_pwm_pin, 0);
+      
     
