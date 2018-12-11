15c64ff5bc1ed2685c5ed5ccf58058ae95114a98
pcmola <pcmola@naver.com>
2018-12-11 21:02:47 +0900

    Update start_clock.sh
    시/분 버튼 눌렀을 때 시간 조정하는 프로그램 실행 쉘 추가

--- start_clock.sh
+++ start_clock.sh
@@ -4,9 +4,11 @@
 #sound start
 /home/pi/c/start_music.sh
 
+#time_change start
+/home/pi/c/start_time_change.sh
+
 #alarm start
 #can't operate alarm&web control simultaneouly
 #/home/pi/py/start_alarm.sh
-
 
 
